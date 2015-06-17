2f5783787769a0546070891a4d96e7b57b44572e
pijoules <lchan1994@yahoo.com>
2015-06-17 17:11:20 -0400

    adding sign up page and corrected adviser missplelling

--- __init__.py
+++ __init__.py
@@ -14,13 +14,16 @@ from private.MongoClientConnection import MongoClientConnection
 client = MongoClientConnection().connection.SDP
 
 # Root directory
-@app.route('/')
+@app.route("/")
 def index():
-	projects = list(client.projects.find(limit=6))
+	projects = list(client.projects.find(limit=5))
 	students = list(client.students.find(limit=4))
-	advisors = list(client.advisors.find(limit=4))
-	return render_template("index.html", projects=projects, students=students, advisors=advisors)
+	advisers = list(client.advisers.find(limit=4))
+	return render_template("index.html", projects=projects, students=students, advisers=advisers)
 
+@app.route("/signup/")
+def signup():
+	return render_template("signup.html")
 
 if __name__ == '__main__':
     app.run()
--- setup.sh
+++ setup.sh
@@ -6,6 +6,3 @@ rm -rf lib/!(README.md)
 
 # Install python dependencies
 pip install -r requirements.txt -t lib/
-
-# Clear anything in the private/ directory
-rm -rf private/!(README.md|__init__.py)
