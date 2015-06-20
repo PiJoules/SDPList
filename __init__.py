8b4b12cd9ebfc4e3bbcfb33a6037debeafb4357e
pijoules <lchan1994@yahoo.com>
2015-06-19 22:51:16 -0400

    working onn editing settings

--- __init__.py
+++ __init__.py
@@ -58,6 +58,12 @@ def index():
 		signed_in="linkedin_token" in session
 	)
 
+@app.route("/account/")
+def account():
+	return render_template("settings.html",
+		signed_in="linkedin_token" in session
+	)
+
 @app.route("/signup/")
 def signup():
 	if "linkedin_token" in session:
@@ -77,19 +83,19 @@ def linkedin_authorize():
             request.args['error_description']
         )
     session['linkedin_token'] = (resp['access_token'], '')
-    #me = linkedin.get('people/~')
-    #my_id = me.data["id"]
+
     me = linkedin.get("people/~:(id,email-address,first-name,last-name,headline,picture-url,industry,summary,specialties,positions:(id,title,summary,start-date,end-date,is-current,company:(id,name,type,size,industry,ticker)),educations:(id,school-name,field-of-study,start-date,end-date,degree,activities,notes),associations,interests,num-recommenders,date-of-birth,publications:(id,title,publisher:(name),authors:(id,name),date,url,summary),patents:(id,title,summary,number,status:(id,name),office:(name),inventors:(id,name),date,url),languages:(id,language:(name),proficiency:(level,name)),skills:(id,skill:(name)),certifications:(id,name,authority:(name),number,start-date,end-date),courses:(id,name,number),recommendations-received:(id,recommendation-type,recommendation-text,recommender),honors-awards,three-current-positions,three-past-positions,volunteer)")
-    #return jsonify(me=me.data)
     data = me.data
+
+    # Add new user
     if client.people.find({"email": data["emailAddress"]}).count() <= 0:
     	client.people.insert({
     		"email": data["emailAddress"],
     		"firstName": data["firstName"],
     		"lastName": data["lastName"],
-    		"photo": data["pictureUrl"],
     		"joined": datetime.now()
     	})
+
     return redirect("/")
 
 @linkedin.tokengetter
